8443b7d629b27977aab1b273c5ddbc698b0e109f
Tyler Walker <twalkerweb@gmail.com>
2018-01-12 16:10:45 -0800

    more changes for modularity

--- src/components/students/students.js
+++ src/components/students/students.js
@@ -3,6 +3,27 @@ import React, { Component } from 'react';
 import Student from './student'
 
 class Students extends Component {
+  constructor() {
+    super() 
+
+    this.state = { students: [
+      {name: "Tyler", grade: "F"},
+      {name: "Jim", grade: "A"},
+      {name: "James", grade: "B"},
+      {name: "Jac", grade: "D"}]
+    };
+  }
+
+  renderStudents() {
+    let students = [];
+
+    for(let i=0; i < this.state.students.length; i++) {
+      students.push(<Student name={this.state.students[i].name} grade={this.state.students[i].grade} />)
+    }
+
+    return students;
+  }
+
   render() {
     return (
       <div className="Students">
@@ -15,7 +36,7 @@ class Students extends Component {
             </tr>
           </thead>
           <tbody>
-            <Student name="Tyler" grade="F" />
+            {this.renderStudents()}
           </tbody>
         </table>
       </div>
